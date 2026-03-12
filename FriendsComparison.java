73b7ab4b5a42ff6b6f148978799da8c9d6f72317
DJaiAditya <jaiad16007@gmail.com>
2026-03-12 14:52:40 +0530

    a

---
+++ FriendsComparison.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class FriendsComparison {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        String n[]={"Amar","Akbar","Anthony"};
+        int age[]=new int[3];
+        double h[]=new double[3];
+
+        for(int i=0;i<3;i++){
+            System.out.print("Age of "+n[i]+": ");
+            age[i]=sc.nextInt();
+            System.out.print("Height of "+n[i]+": ");
+            h[i]=sc.nextDouble();
+        }
+
+        int y=0,t=0;
+
+        for(int i=1;i<3;i++){
+            if(age[i]<age[y]) y=i;
+            if(h[i]>h[t]) t=i;
+        }
+
+        System.out.println("Youngest: "+n[y]);
+        System.out.println("Tallest: "+n[t]);
+    }
+}
